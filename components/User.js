46412f3c3fcf93b1b3bf13c597305f34a8dbdfb9
abha <abha@example.com>
2023-03-06 14:29:03 +0530

    body section added

---
+++ components/User.js
@@ -0,0 +1,33 @@
+import { useSession, signIn, signOut } from "next-auth/react";
+
+const User = () => {
+  const { data: session } = useSession();
+
+  if (session) {
+    return (
+      <>
+        <img
+          onClick={signOut}
+          src={session.user.image}
+          alt=""
+          className="h-10 w-10 rounded-full hover:bg-gray-200 cursor-pointer
+        p-1"
+        />
+      </>
+    );
+  }
+
+  return (
+    <>
+      <button
+        className="bg-blue-500 text-white px-6 py-2 font-medium rounded-md
+      hover:brightness-105 hover:shadow-md"
+        onClick={signIn}
+      >
+        Sign In
+      </button>
+    </>
+  );
+};
+
+export default User;
