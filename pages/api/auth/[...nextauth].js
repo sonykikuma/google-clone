2e1a86ba719804d199ec88feca02315764920731
abha <abha@example.com>
2023-03-08 14:05:51 +0530

    fixes

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -9,7 +9,7 @@ export const authOptions = {
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     }), // ...add more providers here
   ],
-
+  secret: process.env.SECRET,
   pages: {
     signIn: "/auth/signin",
   },
